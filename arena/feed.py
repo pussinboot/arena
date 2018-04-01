1d87c9b375273c39ac497e255b0244e030fb2a8e
pussinboots <pussinboots@cock.li>
2018-03-31 20:11:49 -0400

    fix feeds

--- arena/feed.py
+++ arena/feed.py
@@ -6,5 +6,4 @@ class Feed(Resource):
 
     def __call__(self, offset=0):
         page = self._get('', params={'offset': offset}, auth=True)
-        items = [resource_for_data(d) for d in page.pop('items')]
         return items, page
